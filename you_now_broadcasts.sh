d891e7c0a3ae087a44f34a302dad4c120498d356
Charlie Phillips <charliebphillips@gmail.com>
2017-04-18 21:43:30 -0400

    removed moments requirement

--- you_now_broadcasts.sh
+++ you_now_broadcasts.sh
@@ -17,14 +17,14 @@ function downloadPreviousBroadcastsMenu()
     local user_id=$2
     local ex="false"
 
-    wget --no-check-certificate -q "https://www.younow.com/php/api/moment/profile/channelId=${user_id}/createdBefore=0/records=50" -O "./_temp/${user_name}_moments.json"
-    parseMomentJson "./_temp/${user_name}_moments.json"
-    rm ./_temp/${user_name}_moments.json 
+    wget --no-check-certificate -q "https://www.younow.com/php/api/moment/profile/channelId=${user_id}/createdBefore=0/records=50" -O "./_temp/${user_name}_broadcast.json"
+    parseBroadcastJson "./_temp/${user_name}_broadcast.json"
+    rm ./_temp/${user_name}_broadcast.json 
 
     while [ "$ex" == "false" ]; do
-        displayUserBroadcastWithMoments $user_name
+        displayUserBroadcasts $user_name
 
-        echo "Enter the ID to see available moments."
+        echo "Enter the ID to download broadcast."
         read input_broadcast
 
         if [ "$input_broadcast" == "" ]; then
@@ -33,6 +33,7 @@ function downloadPreviousBroadcastsMenu()
             echo " "
 #            displayBroadcastsMoments $input_broadcast
             broadcast_id=${moments[ $[ ${input_broadcast} * ${columnsM} + 1 ] ]}
+
             ddate=${moments[ $[ ${input_broadcast} * ${columnsM} + 2 ] ]}
 #            
 #            local inner_ex="false"
@@ -68,16 +69,16 @@ function downloadPreviousBroadcastsMenu()
 # @param: user name
 # @param(global): moments
 # @param(global): moments_count
-function displayUserBroadcastWithMoments() 
+function displayUserBroadcasts() 
 {
-    printf "Broadcasts with Moments Available for $user_name \n"
+    printf "Broadcasts Available for $user_name \n"
     printf "    ID    Broadcast id              Date           Moment count"
     local counter="0"
     while [ $counter -lt "${moments_count}" ]
     do
         if [ "$mac" == "" ]
         then
-           printf "  %4s    %10s        %10s      %3s \n" ${moments[$[ $counter * ${columnsM} + 0 ]]} ${moments[$[ $counter * ${columnsM} + 1 ]]} `date -d @${moments[$[ $counter * ${columnsM} + 2 ]]} +%h_%d_%G_%T 2>/dev/null` ${moments[$[ $counter * ${columnsM} + 3 ]]}
+           printf "  %4s    %10s        %10s      %3s \n" ${moments[$[ $counter * ${columnsM} + 0 ]]} ${moments[$[ $counter * ${columnsM} + 1 ]]} `date -d @${moments[$[ $counter * ${columnsM} + 2 ]]} +%h_%d_%G_%T 2>/dev/null` ${moments[$[ $counter * ${columnsM} + 3 ]]}		
         else
            printf "  %4s    %10s        %10s      %3s \n" ${moments[$[ $counter * ${columnsM} + 0 ]]} ${moments[$[ $counter * ${columnsM} + 1 ]]} `date -r ${moments[$[ $counter * ${columnsM} + 2 ]]} +%h_%d_%G_%T 2>/dev/null` ${moments[$[ $counter * ${columnsM} + 3 ]]}
         fi
@@ -109,18 +110,18 @@ function displayBroadcastsMoments()
 # @param: moments json file
 # @return(global -> moments): Array of details about the available moments
 # @return(gloabl -> moments_count): The number of broadcasts with available moments to be downloaded
-function parseMomentJson() 
+function parseBroadcastJson() 
 {
-    local moment_json_file=$1
+    local broadcast_json_file=$1
     unset moments   
     local counter="0"
     local index="1"
 
     ############# read the moment information retrieved from the server #############
-    local broadcast_ids=$(xidel -q -e '($json).items()/join((broadcastId),"-")' "./$moment_json_file" | tr "\n" " ")
+    local broadcast_ids=$(xidel -q -e '($json).items()/join((broadcastId),"-")' "./$broadcast_json_file" | tr "\n" " ")
     broadcast_ids=( $broadcast_ids )
 
-    local ddate=$(xidel -q -e '($json).items()/join((created),"-")' "./$moment_json_file" | tr "\n" " ")
+    local ddate=$(xidel -q -e '($json).items()/join((created),"-")' "./$broadcast_json_file" | tr "\n" " ")
     ddate=( $ddate )
 
     ############# for each broadcast #############
@@ -128,12 +129,12 @@ function parseMomentJson()
     do
         ############# gather up details about this broadcast's moments #############
         moment_index=$[${counter} +1]
-        broadcast_moment_ids=$(xidel -q -e '($json).items('${moment_index}').momentsIds()' "./$moment_json_file" | tr "\n" " ")
+        broadcast_moment_ids=$(xidel -q -e '($json).items('${moment_index}').momentsIds()' "./$broadcast_json_file" | tr "\n" " ")
         broadcast_moment_ids=( $broadcast_moment_ids )
         broadcast_moment_count="${#broadcast_moment_ids[@]}"
         
-        if [ "${broadcast_moment_count}" -ne 0 ]
-        then
+#        if [ "${broadcast_moment_count}" -ne 0 ]
+#        then
             # four entries per broadcast
             moments[$[ ${index} * ${columnsM} + 0 ]]="${index}"
             moments[$[ ${index} * ${columnsM} + 1 ]]="${broadcast_ids[$counter]}"
@@ -141,7 +142,7 @@ function parseMomentJson()
             moments[$[ ${index} * ${columnsM} + 3 ]]="${broadcast_moment_count}"
             moments[$[ ${index} * ${columnsM} + 4 ]]=$(IFS=, ; echo "${broadcast_moment_ids[*]}")
             index=$[$index + 1]
-        fi
+ #       fi
         counter=$[$counter + 1]
     done
     moments_count=${index}
